b31aa1ffbb60fca8e89d2fcef8f67b894fbed66f
깡단 <yyb19920702@gmail.com>
2026-04-03 11:00:38 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if (angle > 0 && angle < 90){
+            answer = 1;
+        }
+        else if(angle == 90)
+        {
+            answer = 2;
+        }
+        else if(angle > 90 && angle < 180)
+        {
+            answer = 3;
+        }
+        else if(angle == 180)
+        {
+            answer = 4;
+        }
+        else{
+            System.out.println("잘못된 입력값입니다");
+        }
+
+        return answer;
+    }
+}
