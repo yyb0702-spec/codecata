0b203902a6892c406d3e8c19730d56b895a056ac
깡단 <yyb19920702@gmail.com>
2026-04-03 10:43:52 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.02 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (num1 > 100 || num1 < 0 || num2 > 100 || num2 < 0) {
+            System.out.println(" 잘못된 입력값입니다");
+        }
+        answer = num1 * num2;
+        return answer;
+    }
+}
